dfdd056c47e12f4e6a4f2f508574d846d23c87ab
Harsh Yadav <harsh48881@gmail.com>
2026-09-15 23:23:50 +0530

    basic recursion for DSA

---
+++ src/O3_Recursion/O4_Sum_First_N_numbers.java
@@ -0,0 +1,22 @@
+package O3_Recursion;
+import java.util.Scanner;
+
+public class O4_Sum_First_N_numbers {
+
+    static int sum(int N){              // here return is nessecery because of int.
+        if(N<1){
+            return 0;
+        }
+        return N +sum(N-1);            // present N + calling same func with N-1.
+    }
+
+
+
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        System.out.println(sum(N));              // calling a function.
+
+    }
+}
